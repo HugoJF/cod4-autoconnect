305ab17ff505d6c617f393d8a9656a8f6e3caac5
Hugo JF <hugojellerferreira@hotmail.com>
2012-10-07 18:46:00 -0300

    Initial commit.
    Created main class, and main features.

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String args[]) {
+		new Main();
+	}
+	
+	public Main() {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//get previous connected servers
+		
+		//get what server id or ip user wants to connect
+		
+		//validate informations
+		
+		//loop getting informations if valid
+		
+		//once empty slot connect
+		
+		//quit program
+		
+	}
+}
