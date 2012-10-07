55b1a5ecdf0425c2348b525840e8fa5aedc35f0d
Hugo JF <hugojellerferreira@hotmail.com>
2012-10-07 19:02:55 -0300

    Defined main steps on main class.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 
@@ -7,18 +8,48 @@ public class Main {
 	}
 	
 	public Main() {
-		
+		ServerInformationParser sip = new ServerInformationParser();
+		Server sv = new Server();
+		AutoConnectOptions aco = new AutoConnectOptions();
+		COD4Launcher cod4l = new COD4Launcher();
+		aco.loadOptionsFromFile("options.xml");
+		if(!aco.loadedOptionsFromFile) {
+			aco.createDefaultOptionsFile();
+		}
 		Scanner scanner = new Scanner(System.in);
 		
 		//get previous connected servers
+		ArrayList<String> previousServers = aco.getPreviousConnectedServers();
+		for(int i = 0; i < previousServers.size(); i++) {
+			System.out.println((i + 1) + ". " + previousServers.get(i));
+		}
 		
 		//get what server id or ip user wants to connect
+		while(true) {
+			String userInput = scanner.nextLine();
+			sv.loadFromIp(userInput);
+			//validate informations
+			sv.validate();
+			if(sv.isValid && sip.validateInformation(sv)) {
+				break;
+			}
+		}
+		if(aco.saveServersToFile){
+			aco.saveServerToFile(sv);
+		}
 		
-		//validate informations
+		while(true) {
+			//loop getting informations if valid
+			sip.loadFromServer(sv);
+			sip.refreshInfo();
+			sip.parseInfo();
+			//once empty slot connect
+			if(sip.hasEmptySlot) {
+				cod4l.launchOnIp(sv.getIp());
+				System.exit(0);
+			}
+		}
 		
-		//loop getting informations if valid
-		
-		//once empty slot connect
 		
 		//quit program
 		
